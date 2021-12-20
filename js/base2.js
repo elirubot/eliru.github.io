970fbfa211d80e1ee8c157eacf19707b46525e63
elirubot <79316365+elirubot@users.noreply.github.com>
2021-12-20 13:20:42 +0400

    Update base2.js

--- js/base2.js
+++ js/base2.js
@@ -312,5 +312,19 @@ for (var a=0;a<paesia.length;a++) {
     
 }
 
+function checkCookies(){
+    let cookieDate = localStorage.getItem('cookieDate');
+    let cookieNotification = document.getElementById('cookie_notification');
+    let cookieBtn = cookieNotification.querySelector('.cookie_accept');
 
+    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+        cookieNotification.classList.add('show');
+    }
+ 
+    cookieBtn.addEventListener('click', function(){
+        localStorage.setItem( 'cookieDate', Date.now() );
+        cookieNotification.classList.remove('show');
+    })
+}
+checkCookies();
 
